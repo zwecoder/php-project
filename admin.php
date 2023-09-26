a76e060fa0f3294d78920cb7d63e016e27c7cfc9
zwecoder <zwewathone77@gmail.com>
2023-09-26 18:40:15 +0630

    admin post create

--- admin.php
+++ admin.php
@@ -3,16 +3,27 @@
        include_once "view/top.php";
      include_once "view/nav.php";
     include_once "view/header.php";
+    include_once "systemgen/postuploader.php";
     if(checkSession("username")){
        if(getsession("username")==="zwewathone"){
 
+            if(isset($_POST['submit'])){
+                $posttitle=$_POST["posttitle"];
+                $postwriter=$_POST["postwriter"];
+                $posttype=$_POST["posttype"];
+                $postcontent=$_POST["postcontent"];
+                $imglink=mt_rand(time(),time())."-".$_FILES["file"]["name"].mt_rand(time(),time());
+                move_uploaded_file($_FILES['file']['tmp_name'],'assets/upload/'.$imglink);
+                
+               $bol = insertPost($posttitle,$postwriter,$posttype,$postcontent,$imglink);
+                echo $bol;
+            }
        ?>
        <div class="container my-3">
     <div class="row">
         <?php include_once "view/sidebar.php"?>
       <section class="col-sm-9">
-       <form enctype="multipart/form-data" class="border p-4 rounded mb-5">
-        <h2 class="text-center text-primary english">Admin Post</h2>
+       <form method="POST" action="<?php $_PHP_SELF ?>" enctype="multipart/form-data" class="border p-4 rounded mb-5">
         <h2 class="text-center text-primary english">Admin Post</h2>
             <div class="form-group">
                 <label for="posttitle" class="english">Post Title</label>
@@ -21,7 +32,7 @@
                 <input type="text" class="form-control english" id="postwriter" name="postwriter">
             </div>
             <label for="posttype" class="english">Post Type</label>
-                <select class="form-select english" aria-label="select post type">
+                <select class="form-select english" name="posttype" aria-label="select post type">
                     <option selected> select menu</option>
                     <option value="1">free</option>
                     <option value="2">paid</option>
---
+++ systemgen/postuploader.php
@@ -0,0 +1,15 @@
+<?php
+require_once "db_connect.php";
+
+function insertPost($posttitle,$postwriter,$posttype,$postcontent,$imglink){
+   $created_at = created_at();
+   $db=dbConnect();
+  
+   $qry ="INSERT INTO post (posttitle,postwriter,posttype,postcontent,imglink,created_at)
+        VALUES
+        ('$posttitle','$postwriter',$posttype,'$postcontent','$imglink','$created_at')";
+   $result=mysqli_query($db,$qry);
+   return $result?"True":"false"; 
+}
+
+?>
